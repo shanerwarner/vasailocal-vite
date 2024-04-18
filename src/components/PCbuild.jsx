2949783613965a7c195aeed062441e476ec07698
Shane Menezes <shane.ryan.menezes@gmail.com>
2024-04-18 16:45:53 +0530

    1

--- src/components/PCbuild.jsx
+++ src/components/PCbuild.jsx
@@ -3,69 +3,69 @@ export default function PCbuild() {
         <>
             <main className="px-3 flex-grow container mx-auto pt-10 ">
 
-                <section class="bg-slate-500  dark:bg-gray-800 rounded-lg border dark:text-gray-200">
+                <section class=" dark:bg-gray-800 rounded-lg border dark:text-gray-200 border-2 rounded-lg">
 
                     <div class="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                         <div class="max-w-xl">
                             <h2 class="text-3xl font-bold sm:text-4xl">Custom PC Build 🛠️</h2>
 
-                            <p class="mt-4 text-gray-300">
+                            <p class="mt-5 text-xl font-normal text-gray-500 dark:text-gray-400">
                                 Looking for a PC that matches your needs exactly? Our custom builds are tailored just for you:
                             </p>
                         </div>
 
                         <div class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
                             <div class="flex items-start gap-4">
-                                <span class="shrink-0 rounded-lg bg-stone-700 border p-4">
+                                <span class="shrink-0 rounded-lg bg-blue-300 border-2 p-4">
                                     <h1>💙</h1>
                                 </span>
 
                                 <div>
                                     <h2 class="text-lg font-bold">Personalized:</h2>
 
-                                    <p class="mt-1 text-sm text-gray-300">
+                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
                                         I work with you to understand your needs.
                                     </p>
                                 </div>
                             </div>
 
                             <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-stone-700 border p-4">
+                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
                                     <h1>⭐</h1>
                                 </span>
 
                                 <div>
                                     <h2 class="text-lg font-bold">Quality:</h2>
 
-                                    <p class="mt-1 text-sm text-gray-300">
+                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
                                         Only top components from trusted brands.
                                     </p>
                                 </div>
                             </div>
 
                             <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-stone-700 border p-4">
+                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
                                     <h1>⁉️</h1>
                                 </span>
 
                                 <div>
                                     <h2 class="text-lg font-bold">Expert Guidance: </h2>
 
-                                    <p class="mt-1 text-sm text-gray-300">
+                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
                                         Knowledge to help you make the right choices.
                                     </p>
                                 </div>
                             </div>
 
                             <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-stone-700 border p-4">
-<h1>🕙</h1>
+                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
+                                    <h1>🕙</h1>
                                 </span>
 
                                 <div>
                                     <h2 class="text-lg font-bold">Future-Proof: </h2>
 
-                                    <p class="mt-1 text-sm text-gray-300">
+                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
                                         Designed for easy upgrades.
                                     </p>
                                 </div>
