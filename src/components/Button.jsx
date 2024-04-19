3ac5ee87a9f0aa1ba434ae035cb012cd9e105641
Shane Menezes <shane.ryan.menezes@gmail.com>
2024-04-19 15:11:32 +0530

    2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button() {
         <>
             <div className="pb-12 sm:pb-16 sm:pt-32 lg:pb-24 xl:pb-32 xl:pt-40">
                 <div class="flex items-center justify-center ">
-                    <a href="#_" class="relative inline-block text-lg group">
+                    <a href="https://wa.me/7875329339?text=I%20Require%20Assistance" class="relative inline-block text-lg group">
                         <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                             <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
                             <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
