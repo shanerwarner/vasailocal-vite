3a1cfa8867ebd81ef8ff6fa3e03e73721d920b49
Shane Menezes <shane.ryan.menezes@gmail.com>
2024-04-19 14:12:32 +0530

    2

--- src/components/PCbuild.jsx
+++ src/components/PCbuild.jsx
@@ -2,36 +2,33 @@ export default function PCbuild() {
     return (
         <>
             <main className="px-3 flex-grow container mx-auto pt-10 ">
+
             <div class="bg-white py-6 sm:py-8 lg:py-12">
   <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-
     <div class="mb-10 md:mb-16">
       <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Custom PC Build 🛠️</h2>
 
       <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Looking for a PC that matches your needs exactly? Our custom builds are tailored just for you:</p>
     </div>
 
-
     <div class="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
-
       <div class="flex gap-4 md:gap-6">
         <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
           </svg>
         </div>
 
         <div>
           <h3 class="mb-2 text-lg font-semibold md:text-xl">Personalized</h3>
           <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
-          
         </div>
       </div>
 
       <div class="flex gap-4 md:gap-6">
         <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
           </svg>
         </div>
 
@@ -45,12 +42,12 @@ export default function PCbuild() {
       <div class="flex gap-4 md:gap-6">
         <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
           </svg>
         </div>
 
         <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Cloud</h3>
+          <h3 class="mb-2 text-lg font-semibold md:text-xl">Expert Guidance</h3>
           <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
           <a href="#" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</a>
         </div>
@@ -59,12 +56,12 @@ export default function PCbuild() {
       <div class="flex gap-4 md:gap-6">
         <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              <path stroke-linecap="round" stroke-width="2" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
         </div>
 
         <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Speed</h3>
+          <h3 class="mb-2 text-lg font-semibold md:text-xl">Future-Proof</h3>
           <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
           <a href="#" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</a>
         </div>
@@ -78,101 +75,15 @@ export default function PCbuild() {
         </div>
 
         <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Support</h3>
+          <h3 class="mb-2 text-lg font-semibold md:text-xl">Extended Support</h3>
           <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
           <a href="#" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</a>
         </div>
       </div>
-
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-          </svg>
-        </div>
-
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Dark Mode</h3>
-          <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
-          <a href="#" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</a>
-        </div>
-      </div>
-
     </div>
   </div>
 </div>
 
-                <section class=" dark:bg-gray-800 rounded-lg border dark:text-gray-200 border-2 rounded-lg">
-
-                    <div class="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                        <div class="max-w-xl">
-                            <h2 class="text-3xl font-bold sm:text-4xl">Custom PC Build 🛠️</h2>
-
-                            <p class="mt-5 text-xl font-normal text-gray-500 dark:text-gray-400">
-                                Looking for a PC that matches your needs exactly? Our custom builds are tailored just for you:
-                            </p>
-                        </div>
-
-                        <div class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
-                            <div class="flex items-start gap-4">
-                                <span class="shrink-0 rounded-lg bg-blue-300 border-2 p-4">
-                                    <h1>💙</h1>
-                                </span>
-
-                                <div>
-                                    <h2 class="text-lg font-bold">Personalized:</h2>
-
-                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                                        I work with you to understand your needs.
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
-                                    <h1>⭐</h1>
-                                </span>
-
-                                <div>
-                                    <h2 class="text-lg font-bold">Quality:</h2>
-
-                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                                        Only top components from trusted brands.
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
-                                    <h1>⁉️</h1>
-                                </span>
-
-                                <div>
-                                    <h2 class="text-lg font-bold">Expert Guidance: </h2>
-
-                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                                        Knowledge to help you make the right choices.
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class="flex items-start gap-4">
-                                <span class="hrink-0 rounded-lg bg-blue-300 border-2  p-4">
-                                    <h1>🕙</h1>
-                                </span>
-
-                                <div>
-                                    <h2 class="text-lg font-bold">Future-Proof: </h2>
-
-                                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                                        Designed for easy upgrades.
-                                    </p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </section>
             </main>
         </>
     )
