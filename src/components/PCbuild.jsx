f417f2dae8d9bdedb7be123157326208831549fe
Shane Menezes <shane.ryan.menezes@gmail.com>
2024-04-19 14:48:11 +0530

    32

--- src/components/PCbuild.jsx
+++ src/components/PCbuild.jsx
@@ -3,97 +3,99 @@ export default function PCbuild() {
         <>
             <main className="px-3 flex-grow container mx-auto pt-10 ">
 
-            <div class="bg-white py-6 sm:py-8 lg:py-12">
-  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-    <div class="mb-10 md:mb-16">
-      <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Custom PC Build 🛠️</h2>
+                <div class="bg-white py-6 sm:py-8 lg:py-12">
+                    <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                        <div class="relative mx-auto mt-20 mb-11 max-w-xl">
+                            <img class="h-64 w-full rounded-md object-cover" src="https://images.unsplash.com/photo-1628358011414-fb7ddaca0dbb?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Random image" />
+                            <div class="absolute inset-0 rounded-md bg-gray-700 opacity-60"></div>
+                            <div class="absolute inset-0 flex items-center justify-center">
+                                <h2 class="text-3xl font-bold text-white">Custom PC Builds</h2>
+                            </div>
+                        </div>
 
-      <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Looking for a PC that matches your needs exactly? Our custom builds are tailored just for you:</p>
-    </div>
+                        <div class="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                    </svg>
+                                </div>
 
-    <div class="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-          </svg>
-        </div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Personalized</h3>
+                                    <p class="mb-2 text-gray-500">I work with you to understand your needs.</p>
+                                </div>
+                            </div>
 
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Personalized</h3>
-          <p class="mb-2 text-gray-500">I work with you to understand your needs.</p>
-        </div>
-      </div>
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                </div>
 
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-          </svg>
-        </div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Quality</h3>
+                                    <p class="mb-2 text-gray-500">Only top components are used from trusted brands.</p>
+                                </div>
+                            </div>
 
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Quality</h3>
-          <p class="mb-2 text-gray-500">Only top components are used from trusted brands.</p>
-        </div>
-      </div>
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                                    </svg>
+                                </div>
 
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-          </svg>
-        </div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Expert Guidance</h3>
+                                    <p class="mb-2 text-gray-500">Knowledge to help you make the right choices.</p>
 
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Expert Guidance</h3>
-          <p class="mb-2 text-gray-500">Knowledge to help you make the right choices.</p>
+                                </div>
+                            </div>
 
-        </div>
-      </div>
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-width="2" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </div>
 
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-width="2" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-          </svg>
-        </div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Future-Proof</h3>
+                                    <p class="mb-2 text-gray-500">tailor component choices and configurations to ensure your PC remains relevant and high-performing for years to come. Let me help you build a PC ready for the future.</p>
 
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Future-Proof</h3>
-          <p class="mb-2 text-gray-500">tailor component choices and configurations to ensure your PC remains relevant and high-performing for years to come. Let me help you build a PC ready for the future.</p>
+                                </div>
+                            </div>
 
-        </div>
-      </div>
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+                                    </svg>
+                                </div>
 
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Extended Support</h3>
+                                    <p class="mb-2 text-gray-500">Ongoing assistance for your computer needs, from hardware troubleshooting to software optimization to ensure your PC runs smoothly and efficiently, tailored to your requirements.</p>
 
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Extended Support</h3>
-          <p class="mb-2 text-gray-500">Ongoing assistance for your computer needs, from hardware troubleshooting to software optimization to ensure your PC runs smoothly and efficiently, tailored to your requirements.</p>
+                                </div>
+                            </div>
+                            <div class="flex gap-4 md:gap-6">
+                                <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+                                    </svg>
+                                </div>
 
-        </div>
-      </div>
-      <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </div>
-
-        <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">Budget Builds</h3>
-          <p class="mb-2 text-gray-500">Expert guidance for cost-effective PC setups. I help you maximize performance within your budget constraints, from component selection to assembly tips. Let me assist you in building a powerful PC without breaking the bank.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+                                <div>
+                                    <h3 class="mb-2 text-lg font-semibold md:text-xl">Budget Builds</h3>
+                                    <p class="mb-2 text-gray-500">Expert guidance for cost-effective PC setups. I help you maximize performance within your budget constraints, from component selection to assembly tips. Let me assist you in building a powerful PC without breaking the bank.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
             </main>
